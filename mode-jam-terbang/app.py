b12a11fd3f20f23c8825e1024fd16a986221faf7
Irfan Anshari <irfanxdev@tuta.io>
2026-02-08 15:15:06 +0800

    Add files via upload

--- mode-jam-terbang/app.py
+++ mode-jam-terbang/app.py
@@ -5,29 +5,29 @@ import os
 def Animasi():
     folder = "frame"
     if not os.path.exists(folder):
-        print(f"folder '{folder}' mboten ditemukan!")
+        print(f"folder '{folder}' tidak ditemukan!")
         return
 
     frames = [
-        {"jeneng_file": "1.txt", "wektu": 0.1},
-        {"jeneng_file": "2.txt", "wektu": 0.1},
-        {"jeneng_file": "3.txt", "wektu": 0.1},
-        {"jeneng_file": "4.txt", "wektu": 0.1},
-        {"jeneng_file": "5.txt", "wektu": 0.1},
-        {"jeneng_file": "4.txt", "wektu": 0.1},
-        {"jeneng_file": "3.txt", "wektu": 0.1},
-        {"jeneng_file": "2.txt", "wektu": 0.1},
+        {"nama_file": "1.txt", "waktu": 0.1},
+        {"nama_file": "2.txt", "waktu": 0.1},
+        {"nama_file": "3.txt", "waktu": 0.1},
+        {"nama_file": "4.txt", "waktu": 0.1},
+        {"nama_file": "5.txt", "waktu": 0.1},
+        {"nama_file": "4.txt", "waktu": 0.1},
+        {"nama_file": "3.txt", "waktu": 0.1},
+        {"nama_file": "2.txt", "waktu": 0.1},
     ]
 
     while True:
         for frame in frames:
-            frame_path = os.path.join(folder, frame["jeneng_file"])
+            frame_path = os.path.join(folder, frame["nama_file"])
             if os.path.exists(frame_path):
                 with open(frame_path, "r") as f:
                     content = f.read()
                 sys.stdout.write("\033c")  
                 sys.stdout.write(content)
                 sys.stdout.flush()
-                time.sleep(frame["wektu"])
+                time.sleep(frame["waktu"])
 
 Animasi()
